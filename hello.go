848b4253187fb4c5a5ce36c2c72172c0e5bafa11
Joby Poriyath <jobyp@xilinx.com>
2021-10-25 10:15:18 +0100

    Hello World

--- hello.go
+++ hello.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Learning Go!")
+	fmt.Println("Learning Go from https://tour.golang.org")
+	fmt.Println("The time is", time.Now())
 }
-
